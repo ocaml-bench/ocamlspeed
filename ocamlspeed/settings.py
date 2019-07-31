b4929a8b225017b1a9bad1308b96afa58f5fd109
Sadiq <sadiq@toao.com>
2019-07-31 17:33:50 +0100

    upgrade middleware to fix admin

--- ocamlspeed/settings.py
+++ ocamlspeed/settings.py
@@ -44,13 +44,15 @@ SILENCED_SYSTEM_CHECKS = [
     'admin.E410',
 ]
 
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-)
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 
 ROOT_URLCONF = '{0}.urls'.format(TOPDIR)
 
